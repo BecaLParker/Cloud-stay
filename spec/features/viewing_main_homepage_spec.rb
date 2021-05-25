b86f83c8b1d57178f25203df8b07b28cb3424778
BecaLParker <becaparker@gmail.com>
2021-05-25 12:02:37 +0100

    Button 'Add a new cloud listing'

--- spec/features/viewing_main_homepage_spec.rb
+++ spec/features/viewing_main_homepage_spec.rb
@@ -3,10 +3,13 @@
 require 'capybara/rspec'
 require_relative '../../app'
 
-feature 'viewing availble clouds' do
+feature 'viewing available clouds' do
   scenario 'visits the clouds page' do
     # code here for capybara to login first
     visit('/clouds')
     expect(page).to have_content('Cloud nine')
+    expect(page).to have_button('Add a cloud listing')
   end
+
+
 end
