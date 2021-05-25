12948dd2a8d5be5ed7611ba77d16431d53d63ae4
BecaLParker <becaparker@gmail.com>
2021-05-25 14:24:16 +0100

    page links feature WIP

--- spec/features/viewing_main_homepage_spec.rb
+++ spec/features/viewing_main_homepage_spec.rb
@@ -3,19 +3,20 @@
 require 'capybara/rspec'
 require_relative '../../app'
 
-feature 'viewing available clouds' do
-  scenario 'visits the clouds page' do
-    # code here for capybara to login first
+feature 'visiting the clouds listing page' do
+      # before block and code here for capybara to login first
+
+  it 'shows available clouds' do
     visit('/clouds')
-    expect(page).to have_button('Add a cloud listing')
     expect(page).to have_table('Available clouds')
     expect(page).to have_content('Cloud nine')
-    # expect(page).to have_link('Sign out', href: 'http://www.makersacademy.com')
-    # expect(page).to have_link('Requests', href: 'http://www.makersacademy.com')
-    # expect(page).to have_link('Clouds', href: 'http://www.makersacademy.com')
-
-
   end
 
+  it 'shows page links' do
+    visit('/clouds')
+    expect(page).to have_button('Add a cloud listing')
+    expect(page).to have_link('Sign out', href: '/')
+    expect(page).to have_link('Requests', href: '/requests')
+  end
 
 end
