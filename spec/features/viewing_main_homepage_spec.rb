7d94823c8b392cf48e161ba9739dc61c40bffbf7
BecaLParker <becaparker@gmail.com>
2021-05-25 13:59:24 +0100

    Table format for listings display

--- spec/features/viewing_main_homepage_spec.rb
+++ spec/features/viewing_main_homepage_spec.rb
@@ -8,8 +8,13 @@ feature 'viewing available clouds' do
     # code here for capybara to login first
     visit('/clouds')
     expect(page).to have_button('Add a cloud listing')
-    expect(page).to have_table
+    expect(page).to have_table('Available clouds')
     expect(page).to have_content('Cloud nine')
+    # expect(page).to have_link('Sign out', href: 'http://www.makersacademy.com')
+    # expect(page).to have_link('Requests', href: 'http://www.makersacademy.com')
+    # expect(page).to have_link('Clouds', href: 'http://www.makersacademy.com')
+
+
   end
 
 
