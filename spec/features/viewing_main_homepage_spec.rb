55fdec1e5bdc699c2e21892194bd8ef0387dcc35
BecaLParker <becaparker@gmail.com>
2021-05-25 12:32:27 +0100

    Table format for listings display

--- spec/features/viewing_main_homepage_spec.rb
+++ spec/features/viewing_main_homepage_spec.rb
@@ -7,8 +7,9 @@ feature 'viewing available clouds' do
   scenario 'visits the clouds page' do
     # code here for capybara to login first
     visit('/clouds')
-    expect(page).to have_content('Cloud nine')
     expect(page).to have_button('Add a cloud listing')
+    expect(page).to have_table
+    expect(page).to have_content('Cloud nine')
   end
 
 
