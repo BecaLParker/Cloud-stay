e09938c4f395b73b56d4c76ecde850ed77e6624a
Dewald Viljoen <79870549+Dev-ops-true@users.noreply.github.com>
2021-05-28 14:33:02 +0100

    bookings can now be made

--- app.rb
+++ app.rb
@@ -79,19 +79,20 @@ class CloudStay < Sinatra::Base
     erb :new
   end
 
-  post '/clouds/:cloud_id/book' do
-    @cloud_id = params[:cloud_id]
-    redirect '/clouds/:cloud_id/book/new'
-  end
+  # post '/clouds/:id/book' do
+  #   @cloud_id = params[:id]
+  #   redirect '/clouds/:id/book/new'
+  # end
 
-  get '/clouds/:cloud_id/book/new' do
-    @cloud_id = params[:cloud_id]
-    @available = Availability.find(cloud_id: @cloud_id)
+  get '/clouds/:id/book/new' do
+    @cloud_id = params[:id]
+    @available = Availability.find(cloud_id: params[:id])
     erb :book
   end
 
-  post '/clouds/:cloud_id/book/new' do
-    Book.create(cloud_id: @cloud_id, start_date: params[:start_date], end_date: params[:end_date])
+  post '/clouds/:id/book' do
+    @cloud_id = params[:id]
+    Book.create(start_date: params[:start_date], end_date: params[:end_date], cloud_id: params[:id])
     redirect '/clouds/confirmation'
   end
 
--- lib/availability.rb
+++ lib/availability.rb
@@ -11,7 +11,7 @@ class Availability
   end
 
   def self.create(cloud_id:, start_date:, end_date:)
-    if ENV['ENVIRONMENT'] = 'test'
+    if ENV['ENVIRONMENT'] == 'test'
       connection = PG.connect(dbname: 'cloud_stay_test')
     else
       connection = PG.connect(dbname: 'cloud_stay')
@@ -26,18 +26,18 @@ class Availability
   end    
 
   def self.find(cloud_id:)
-    connection = if ENV['ENVIRONMENT'] == 'test'
-      PG.connect(dbname: 'cloud_stay_test')
-    else
-      PG.connect(dbname: 'cloud_stay')
-    end
-    return nil unless cloud_id
+    connection =  if ENV['ENVIRONMENT'] == 'test'
+                    PG.connect(dbname: 'cloud_stay_test')
+                  else
+                    PG.connect(dbname: 'cloud_stay')
+                  end
+                  return nil unless cloud_id
 
     result = connection.exec("SELECT * FROM availability WHERE id = #{cloud_id}")
-    Availability.new(
-      start_date: result[0]['start_date'],
-      end_date: result[0]['end_date'],
-      cloud_id: result[0]['cloud_id']
+      Availability.new(
+        start_date: result[0]['start_date'],
+        end_date: result[0]['end_date'],
+        cloud_id: result[0]['cloud_id']
     )  
   end
 end
